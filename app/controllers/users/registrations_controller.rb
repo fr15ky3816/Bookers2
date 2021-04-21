d54e6102980b3a89e72e7bd06e28b1c6a5522396
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-21 16:13:23 +0000

    add 4/21

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+   def create
+     super
+      flash[:notice] = "Welcome! You have signed up successfully."
+   end
 
   # GET /resource/edit
   # def edit
