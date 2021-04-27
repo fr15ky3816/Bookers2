785a96a253ce98875a1fe7ebe54eed9b490e0362
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-27 15:24:13 +0000

    added like bottum

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about", as: "about"
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index, :show, :update, :edit]
 
 end
